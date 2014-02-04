800aa7120d2e607a46c52ddf941ac20d1429ef40
Kevin O'Hagan <kevinohagan@gmail.com>
2014-02-04 19:42:51 +0100

    changed menu to sidebar

--- client/views/header/header.js
+++ client/views/header/header.js
@@ -1,13 +1,18 @@
 Template.header.rendered = function () {
   $('.ui.sidebar').sidebar({
-    // overlay: true
+    overlay: true,
+    duration: 1000
   });
 
   $('.ui.dropdown').dropdown();
 };
 
 Template.header.events({
-  'click .button': function () {
+  'click #btn': function () {
+    $('.ui.sidebar').sidebar('toggle');
+  },
+  'click .ui.sidebar .header': function(e) {
+    e.preventDefault();
     $('.ui.sidebar').sidebar('toggle');
   }
 });
--- client/views/test/test.js
+++ client/views/test/test.js
@@ -1,16 +1,8 @@
 Template.test.rendered = function () {
     $('.CoverImage').attr('data-stellar-ratio', 0.5);
-    $('.ui.sidebar').sidebar({
-      overlay: true
-    });
+
 };
 
 Template.test.events({
-  'click #btn': function () {
-    $('.ui.sidebar').sidebar('toggle');
-  },
-  'click .ui.sidebar .header': function(e) {
-    e.preventDefault();
-    $('.ui.sidebar').sidebar('toggle');
-  }
+
 });
